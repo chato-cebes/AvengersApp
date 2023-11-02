22566f7eb77270c08ed214592505d3926e8dc1bf
chato-cebes <javancebes@gmail.com>
2023-11-02 12:52:59 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image, ScrollView, Text } from "react-native";
 
 export default function App() {
+  /* const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require(`./assets/favicon.png`)} />
+      <TextInput
+        placeholder="Escriba para convertir en mayuscula"
+        onChangeText={(text) => setText(text)}
+        defaultValue={text}
+      />
+      <Text>{text.toUpperCase()}</Text>
       <StatusBar style="auto" />
     </View>
+  ); */
+
+  return (
+    <ScrollView>
+      <Text>BlackWidow</Text>
+      <Image source={require("./assets/BlackWidow.png")} />
+      <Text>Captain</Text>
+      <Image source={require("./assets/Captain.png")} />
+      <Text>Hawkeye</Text>
+      <Image source={require("./assets/Hawkeye.png")} />
+      <Text>Hulk</Text>
+      <Image source={require("./assets/Hulk.png")} />
+      <Text>IronMan</Text>
+      <Image source={require("./assets/IronMan.png")} />
+      <Text>Thor</Text>
+      <Image source={require("./assets/Thor.png")} />
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+}); */
